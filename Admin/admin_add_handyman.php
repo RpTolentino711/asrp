8c2bf5cbe795d1fd57e0bf4c8f12117642cdf1ed
itscool <romeopaolotolentino@gmail.com>
2025-09-11 01:07:06 +0800

    PUSH 37

--- Admin/admin_add_handyman.php
+++ Admin/admin_add_handyman.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../database/database.php';
-$db = new Database();
+session_start();
+require_once '../database/database.php';
 
 // Fetch all handymen with their job type
 $handymen = [];
