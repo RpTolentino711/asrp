2fa9860e7d8a20045d6604e849f09e76629b52c3
itscool <romeopaolotolentino@gmail.com>
2025-09-16 00:36:30 +0800

    PUSH 133

--- database/database.php
+++ database/database.php
@@ -1435,7 +1435,6 @@ public function acceptRentalRequest($request_id) {
         LEFT JOIN clientspace cs ON c.Client_ID = cs.Client_ID
         LEFT JOIN space s ON cs.Space_ID = s.Space_ID
         LEFT JOIN invoice i ON i.Client_ID = c.Client_ID AND i.Space_ID = s.Space_ID
-        WHERE (i.Status IS NULL OR i.Status != 'kicked')
         ORDER BY c.Client_ID DESC";
     return $this->runQuery($sql, [], true);
     }
