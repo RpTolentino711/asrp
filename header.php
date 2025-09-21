f4b4116cae8e6974268b5ddd788aace31881db67
itscool <romeopaolotolentino@gmail.com>
2025-09-22 04:39:33 +0800

    PUSH 209

--- header.php
+++ header.php
@@ -735,9 +735,6 @@ $is_logged_in = isset($_SESSION['client_id']);
                 <i class="bi bi-eye"></i>
               </button>
             </div>
-            <div class="text-end mt-1">
-              <a href="forgot_password.php" class="link-secondary small">Forgot Password?</a>
-            </div>
           </div>
           <div class="d-grid">
             <button type="submit" class="modern-btn modern-btn-primary">
@@ -1368,4 +1365,6 @@ function checkRegisterForm() {
   return true;
 }
 </script>
+
+
 
