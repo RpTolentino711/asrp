b1504cfcd06c8640830a88114b061ab1c9273cf9
itscool <romeopaolotolentino@gmail.com>
2025-09-22 16:24:09 +0800

    PUSH 229

--- header.php
+++ header.php
@@ -779,7 +779,8 @@ document.addEventListener('DOMContentLoaded', function() {
   </div>
 </div>
 
-<!-- Forgot Password Modal -->
+
+<!-- Forgot Password Modal (move to root level for proper modal stacking) -->
 <div class="modal fade modern-modal" id="forgotPasswordModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
