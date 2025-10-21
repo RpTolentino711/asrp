ca84a751a4b765958806b6ff72361319901aa6f9
itscool <romeopaolotolentino@gmail.com>
2025-10-21 13:49:21 +0800

    PUSH

--- handyman_type.php
+++ handyman_type.php
@@ -513,7 +513,6 @@ $job_types = $db->getAllJobTypes();
     <?php require('footer.php'); ?>
 
     <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
     <script>
         // Smooth scroll animations
